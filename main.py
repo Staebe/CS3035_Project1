93cf2d6fde0a2b3ec895db87e6ee06988894d754
jenn95 <jennifer.bell@unb.ca>
2024-10-06 21:55:45 -0300

    Added color resets to reset()

--- main.py
+++ main.py
@@ -25,25 +25,16 @@ class TicTacToe(MDApp):
         
     def reset(self):
         self.user = "O"
-        self.root.ids.btnTL.disabled = False;
-        self.root.ids.btnL.disabled = False;
-        self.root.ids.btnBL.disabled = False;
-        self.root.ids.btnTC.disabled = False;
-        self.root.ids.btnC.disabled = False;
-        self.root.ids.btnBC.disabled = False;
-        self.root.ids.btnTR.disabled = False;
-        self.root.ids.btnR.disabled = False;
-        self.root.ids.btnBR.disabled = False;
-            
-        self.root.ids.btnTL.text = "";
-        self.root.ids.btnL.text = "";
-        self.root.ids.btnBL.text = "";
-        self.root.ids.btnTC.text = "";
-        self.root.ids.btnC.text = "";
-        self.root.ids.btnBC.text = "";
-        self.root.ids.btnTR.text = "";
-        self.root.ids.btnR.text = "";
-        self.root.ids.btnBR.text = "";
+        # Reset all button states and colors
+        buttons = [self.root.ids.btnTL, self.root.ids.btnL, self.root.ids.btnBL,
+                self.root.ids.btnTC, self.root.ids.btnC, self.root.ids.btnBC,
+                self.root.ids.btnTR, self.root.ids.btnR, self.root.ids.btnBR]
+        
+        for button in buttons:
+            button.disabled = False
+            button.text = ""
+            button.background_color = "teal"  #initial from .kv file
+            button.color = [1, 1, 1, 1]  #Apparently this initial state too
 
 
 TicTacToe().run()
